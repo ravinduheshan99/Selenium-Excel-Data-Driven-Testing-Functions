d7ab7ad347eb232609963bc06f18974ebe5bc49b
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-04-27 01:07:30 +0530

    Retrieving data from excel based on condition

--- src/test/java/dataDriven.java
+++ src/test/java/dataDriven.java
@@ -1,7 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
-
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -11,11 +10,9 @@ public class dataDriven {
 
 	public static void main(String[] args) throws IOException {
 		
-		
-		//Once column is identified then scan entire testcase column to identify purchase testcase row
 		//After you grab purchase testcase row = pull all the data of that row and feed into test
 		
-		FileInputStream fis = new FileInputStream("D:\\Documents\\Career\\My Projects\\Selenium Projects\\06-Selenium-Excel-Data-Driven-Testing-Functions\\ExcelDriven\\Test Data\\Test Data.xlxs");
+		FileInputStream fis = new FileInputStream("D:\\Documents\\Career\\My Projects\\Selenium Projects\\06-Selenium-Excel-Data-Driven-Testing-Functions\\ExcelDriven\\Test Data\\Test Data.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		
 		int sheetCount = workbook.getNumberOfSheets();
@@ -28,12 +25,20 @@ public class dataDriven {
 				Row firstrow = rows.next(); //Comes to the header row
 				Iterator<Cell> cell= firstrow.cellIterator();
 				
+				int k=0;
+				int columnIndex=0;
+				
 				while(cell.hasNext()) {
 					Cell cellValue = cell.next();
 					if(cellValue.getStringCellValue().equalsIgnoreCase("Test Cases")) {
-						//Desired column
+						columnIndex=k;
 					}
+					
+					k++;
 				}
+				System.out.println(columnIndex);
+				
+				//Once column is identified then scan entire testcase column to identify purchase testcase row
 			}
 		}
 
